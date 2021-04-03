b731b98c09e831cc4be5ac4709d625eea950e264
Chipzstar <chipzstar.dev@googlemail.com>
2021-04-03 17:59:38 +0100

    class definitions for DQN alg

--- PixelCopter.py
+++ PixelCopter.py
@@ -19,18 +19,25 @@
 
 # # Imports
 
-import sys
 import os
+import pickle
+import platform
+import sys
 import time
+from collections import deque
+from pprint import pprint
+
+import keras
 import numpy as np
 import pandas as pd
 import sklearn as sk
 import tensorflow as tf
-import keras
-import platform
-from pprint import pprint
-from ple.games.pixelcopter import Pixelcopter
+from keras import Sequential
+from keras.callbacks import TensorBoard
+from keras.layers import MaxPooling2D, Dense, Dropout, Activation
+from keras.optimizers import Adam
 from ple import PLE
+from ple.games.pixelcopter import Pixelcopter
 
 print(f"Tensor Flow Version: {tf.__version__}")
 print(f"Keras Version: {keras.__version__}")
@@ -60,65 +67,159 @@ class NaiveAgent:
         action_index = np.random.randint(len(self.actions))
         return self.actions[action_index], self.moves[action_index]
 
-    def go_up(self):
-        action = self.actions[0]
-        return action
+
+# Own Tensorboard class
+class ModifiedTensorBoard(TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.create_file_writer(self.log_dir)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrides, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrides
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrides, so won't close writer
+    def on_train_end(self, logs=None):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
+
+class DQNAgent:
+    LEARNING_RATE = 1e-3
+    BATCH_SIZE = 32
+    INPUT_SIZE = 7
+    LAYER_SIZE = 500
+    OUTPUT_SIZE = 2
+    EPSILON = 0.15
+    DECAY_RATE = 0.005
+    MIN_EPSILON = 0.1
+    GAMMA = 0.99
+    MEMORIES = deque()
+    MEMORY_SIZE = 300
+    MODEL_NAME = f"DQN model LR={LEARNING_RATE} BATCH={BATCH_SIZE}"
+
+    # based on documentation, state has 7 features
+    # output is 2 dimensions, 0 = do nothing, 1 = jump
+
+    def __init__(self, screen=False, forcefps=True):
+        self.game = Pixelcopter(width=250, height=250)
+        self.env = PLE(self.game, fps=30, display_screen=screen, force_fps=forcefps)
+        self.env.init()
+        # main model  # gets trained every step
+        self.model = self.create_model()
+        self.action_map = {
+            0: None,
+            1: 119
+        }
+        # Target model this is what we .predict against every step
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        self.replay_memory = deque(maxlen=self.MEMORY_SIZE)
+        self.tensorboard = ModifiedTensorBoard(log_dir=f"logs/{self.MODEL_NAME}-{int(time.time())}")
+        self.target_update_counter = 0
+        self.rewards = []
+
+    def create_model(self):
+        model = Sequential()
+
+        model.add(Dense(32, input_shape=(self.INPUT_SIZE, ), activation="relu"))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(64, activation="relu"))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(64, activation="relu"))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(self.OUTPUT_SIZE, activation='linear'))  # ACTION_SPACE_SIZE = how many choices (9)
+        model.compile(loss="mse", optimizer=Adam(lr=self.LEARNING_RATE), metrics=['accuracy'])
+        return model
+
+    def update_replay_memory(self, state, action, reward, new_state, done):
+        self.MEMORIES.append((state, action, reward, new_state, done))
+        if len(self.MEMORIES) > self.MEMORY_SIZE:
+            self.MEMORIES.popleft()
+
+    def select_action(self, state):
+        # chose random action with probability epsilon
+        if np.random.uniform() < self.EPSILON:
+            action = np.random.randint(self.OUTPUT_SIZE)
+        # otherwise chose epsilon-greedy action from neural net
+        else:
+            action = self.get_predicted_action([state])
+        actual_action = self.action_map[action]
+        return actual_action
+
+    def get_qs(self, state, step):
+        return self.model.predict(np.array(state))
+
+    def _construct_memories(self, replay):
+        states = np.array([a[0] for a in replay])
+        new_states = np.array([a[3] for a in replay])
+        Q = self.model.predict(states)
+        Q_new = self.model.predict(new_states)
+        replay_size = len(replay)
+        X = np.empty((replay_size, self.INPUT_SIZE))
+        Y = np.empty((replay_size, self.OUTPUT_SIZE))
+        for i in range(replay_size):
+            state_r, action_r, reward_r, new_state_r, done_r = replay[i]
+            target = Q[i]
+            target[action_r] = reward_r
+            if not done_r:
+                target[action_r] += self.GAMMA * np.amax(Q_new[i])
+            X[i] = state_r
+            Y[i] = target
+        return X, Y
+
+    def get_predicted_action(self, sequence):
+        prediction = self.model.predict(np.array(sequence))[0]
+        print("Prediction", prediction)
+        return np.argmax(prediction)
+
+    def get_state(self):
+        state = self.env.getGameState()
+        return np.array(list(state.values()))
 
 
 # init
 def main():
-    game = Pixelcopter(width=300, height=300)
-    env = PLE(game, fps=30, display_screen=True)
-    env.init()
-    agent = NaiveAgent(allowed_actions=env.getActionSet())
-    print("All actions", env.getActionSet())
-    print("Start Lives:", env.lives())
-    reward = 0.0
-    nb_frames = 100
-    start_time = time.time()
-
-    for i in range(nb_frames):
-        print("Frame:", env.getFrameNumber())
-        if env.game_over():
+    agent = DQNAgent()
+    agent.env.reset_game()
+    print("State attributes", agent.env.getGameState().keys())
+    print("All actions", agent.env.getActionSet())
+    done = False
+    total_reward = 0.0
+    while not done:
+        if agent.env.game_over():
             print("GAME OVER!")
-            break
-        action, move = agent.pick_action()
-        print("New move:", move)
-        reward = env.act(action)
-        env.saveScreen(f"screenshots/Frame {i + 1}.png")
-        state = env.getGameState()
-        print("State:")
-        pprint(state)
+            done = True
+        state = agent.get_state()
+        print("State:", state)
+        action = agent.select_action(state)
+        print("Action:", action)
+        reward = agent.env.act(action)
         print("Reward:", reward)
-
-    screenState = env.getScreenRGB()
-    print(screenState.shape)
-
-
-def testing():
-    game = Pixelcopter()
-    env = PLE(game, fps=30, display_screen=True)
-    env.init()
-    agent = NaiveAgent(allowed_actions=env.getActionSet())
-    print("All actions", env.getActionSet())
-    print("Start Lives:", env.lives())
-    total_reward = 0
-    start_time = time.time()
-
-    while not env.game_over():
-        duration = int(time.time() - start_time)
-        if duration >= 10:
-            break
-        # chosen action by agent
-        action = agent.go_up()
-        print(action)
-        # agent makes move in the game
-        reward = env.act(action)
         total_reward += reward
-        print(env.getFrameNumber(), action, reward)
-
-    print("GAME OVER!")
-    print("Total Reward", total_reward)
+        # prevent infinite loop
+        done = True
 
 
 if __name__ == "__main__":
